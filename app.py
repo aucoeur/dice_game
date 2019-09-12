2c3becffd8526ad2dd55082655296adc54b121bc
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-09-12 10:52:51 -0700

    Changed horoscope to dice_game

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
 from flask import Flask, request, render_template
-from random import choice, sample
+from random import randint
 
 app = Flask(__name__)
+
+def roll_dice():
+    result = randint(1,6)
+    return result
+
+@app.route('/')
+def index():
+    section = 'Greetings, human'
+    return render_template('index.html',
+        section=section)
+
+
+@app.route('/dice')
+def play_dice_game():
+    name = request.args.get('name')
+    die1 = roll_dice()
+    die2 = roll_dice()
+    
+    if die1 == die2:
+        return "You won! You rolled " + str(die1) + " and " + str(die2) + "."
+
+    return render_template('dice.html',
+        name=name,
+        die1=die1,
+        die2=die2)
 
 if __name__ == '__main__':
     app.run(debug=True)
